553472c28ef46380e52400f3480235f21665ec66
Anna-er <brungilda364@gmail.com>
2023-12-23 04:35:33 +0300

    feat: Support all primitive types in [tab].tsx

--- web-app/client/src/pages/reports/[taskID]/[tab].tsx
+++ web-app/client/src/pages/reports/[taskID]/[tab].tsx
@@ -1,21 +1,34 @@
-import { useRouter } from 'next/router';
-import { TaskContextProvider, useTaskContext } from '@components/TaskContext';
-import dynamic from 'next/dynamic';
+import { TaskContextProvider } from '@components/TaskContext';
 import { ReportsLayout } from '@components/ReportsLayout/ReportsLayout';
-import styles from '@styles/Snippet.module.scss';
 import { useReportsRouter } from '@components/useReportsRouter';
-
+import { useQuery } from '@apollo/client';
+import {
+  getTaskInfo,
+  getTaskInfoVariables,
+} from '@graphql/operations/queries/__generated__/getTaskInfo';
+import { GET_TASK_INFO } from '@graphql/operations/queries/getTaskInfo';
 import reportsConfig from '../../../primitives/reportsConfig';
+import styles from '@styles/Snippet.module.scss';
+
 export default function Page() {
   const { taskID, currentTab, switchTab } = useReportsRouter();
 
 
-  const selectedTab = reportsConfig.FD.tabs.find(tab => currentTab === tab.pathName) || reportsConfig.FD.tabs[0];
+  const { data: taskInfo } = useQuery<getTaskInfo, getTaskInfoVariables>(
+    GET_TASK_INFO,
+    {
+      variables: { taskID },
+    }
+  );
+  const primitiveType = taskInfo?.taskInfo.data.baseConfig.type;
 
-  if (selectedTab) {
-    return <TaskContextProvider><ReportsLayout pageClass={styles.page} containerClass={styles.container}>
-    <selectedTab.component />
-  </ReportsLayout></TaskContextProvider>;
+  if (primitiveType) {
+    const selectedTab = reportsConfig[primitiveType].tabs.find(tab => currentTab === tab.pathName) || reportsConfig[primitiveType].tabs[0];
+    if (selectedTab) {
+      return <TaskContextProvider><ReportsLayout pageClass={styles.page} containerClass={styles.container}>
+        <selectedTab.component />
+      </ReportsLayout>;</TaskContextProvider>
+
+    }
   }
-
 }
