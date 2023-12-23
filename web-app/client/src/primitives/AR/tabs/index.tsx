998f311cf226c03c9beb9cc841c4ac06dc84fa69
Anna-er <brungilda364@gmail.com>
2023-12-23 04:49:10 +0300

    feat: Support AR

---
+++ web-app/client/src/primitives/AR/tabs/index.tsx
@@ -0,0 +1,32 @@
+import ChartIcon from '@assets/icons/chart.svg?component';
+import DatatableIcon from '@assets/icons/datatable.svg?component';
+import DropDownIcon from '@assets/icons/list-dropdown.svg?component';
+import dynamic from 'next/dynamic';
+
+const DatasetSnippetComponent = dynamic(() => import('../../AR/tabs/menuDatasetSnippet'), {
+  ssr: false,
+});
+const InstanceListComponent = dynamic(() => import('../../AR/tabs/menuInstanceList'), {
+  ssr: false,
+});
+
+
+
+import { TabConfig } from '../../reportsConfig';
+
+import React from 'react';
+
+const menuInstanceList: TabConfig = {
+  pathName: 'dependencies',
+  label: 'Instance list',
+  icon: <DropDownIcon />,
+  component: InstanceListComponent,
+};
+const menuDatasetSnippet: TabConfig = {
+  pathName: 'snippet',
+  label: 'Dataset snippet',
+  icon: <DatatableIcon />,
+  component: DatasetSnippetComponent,
+};
+
+export const ARtabs: TabConfig[] = [menuInstanceList, menuDatasetSnippet];
