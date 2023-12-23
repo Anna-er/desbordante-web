6690b7d22c7b834c598532233d22a3de77110a70
Anna-er <brungilda364@gmail.com>
2023-12-23 04:49:47 +0300

    feat: Support MFD

---
+++ web-app/client/src/primitives/MFD/tabs/index.tsx
@@ -0,0 +1,25 @@
+import { ComponentType } from 'react';
+import ChartIcon from '@assets/icons/chart.svg?component';
+import ClusterIcon from '@assets/icons/cluster.svg?component';
+import DatatableIcon from '@assets/icons/datatable.svg?component';
+import DropDownIcon from '@assets/icons/list-dropdown.svg?component';
+import dynamic from 'next/dynamic';
+
+const MFDClustersComponent = dynamic(() => import('../../MFD/tabs/menuMFDClusters'), {
+  ssr: false,
+});
+
+import { TabConfig } from '../../reportsConfig';
+
+import React, { FC } from 'react';
+
+
+const menuMFDClusters: TabConfig = {
+  pathName: 'clusters',
+  label: 'Clusters',
+  icon: <ClusterIcon />,
+  component: MFDClustersComponent,
+};
+
+
+export const MFDtabs: TabConfig[] = [menuMFDClusters];
