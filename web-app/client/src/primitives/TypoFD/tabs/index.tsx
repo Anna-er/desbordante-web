ba2097ddfba288b00ba1f17ae7e4538898018eab
Anna-er <brungilda364@gmail.com>
2023-12-23 04:50:33 +0300

    feat: Support TypoFD

---
+++ web-app/client/src/primitives/TypoFD/tabs/index.tsx
@@ -0,0 +1,40 @@
+import ClusterIcon from '@assets/icons/cluster.svg?component';
+import DatatableIcon from '@assets/icons/datatable.svg?component';
+import DropDownIcon from '@assets/icons/list-dropdown.svg?component';
+import dynamic from 'next/dynamic';
+
+const DatasetSnippetComponent = dynamic(() => import('../../TypoFD/tabs/menuDatasetSnippet'), {
+  ssr: false,
+});
+const InstanceListComponent = dynamic(() => import('../../TypoFD/tabs/menuInstanceList'), {
+  ssr: false,
+});
+const ClustersComponent = dynamic(() => import('../../TypoFD/tabs/menuClusters'), {
+  ssr: false,
+});
+
+
+import { TabConfig } from '../../reportsConfig';
+
+import React from 'react';
+
+const menuClusters: TabConfig = {
+  pathName: 'clusters',
+  label: 'Clusters',
+  icon: <ClusterIcon />,
+  component: ClustersComponent,
+};
+const menuInstanceList: TabConfig = {
+  pathName: 'dependencies',
+  label: 'Instance list',
+  icon: <DropDownIcon />,
+  component: InstanceListComponent,
+};
+const menuDatasetSnippet: TabConfig = {
+  pathName: 'snippet',
+  label: 'Dataset snippet',
+  icon: <DatatableIcon />,
+  component: DatasetSnippetComponent,
+};
+
+export const TypoFDtabs: TabConfig[] = [menuInstanceList, menuClusters, menuDatasetSnippet];
---
+++ web-app/client/src/primitives/TypoFD/tabs/menuDatasetSnippet.tsx
@@ -0,0 +1,69 @@
+import { useQuery } from '@apollo/client';
+import { useMemo, useRef } from 'react';
+import ScrollableTable from '@components/ScrollableTable';
+import { useTaskContext } from '@components/TaskContext';
+import {
+  getDataset,
+  getDatasetVariables,
+} from '@graphql/operations/queries/__generated__/getDataset';
+import { GET_DATASET } from '@graphql/operations/queries/getDataset';
+import styles from '@styles/Snippet.module.scss';
+import { NextPageWithLayout } from 'types/pageWithLayout';
+
+
+const DEFAULT_LIMIT = 30;
+const LIMIT_INCREMENT = 30;
+
+const ReportsSnippet: NextPageWithLayout = () => {
+
+  const { taskID, selectedDependency } = useTaskContext();
+  const paginationLimit = useRef(DEFAULT_LIMIT);
+  const { data } = useQuery<getDataset, getDatasetVariables>(GET_DATASET, {
+    variables: {
+      taskID,
+      pagination: {
+        offset: 0,
+        limit: DEFAULT_LIMIT,
+      },
+    },
+  });
+
+  const snippet = data?.taskInfo.dataset?.snippet
+
+
+
+
+  const handleScrollToBottom = async () => {
+    if (!snippet || paginationLimit.current >= snippet.datasetInfo.rowsCount) {
+      return;
+    }
+
+    paginationLimit.current += LIMIT_INCREMENT;
+    const newRows = data?.taskInfo.dataset?.snippet.rows;
+  };
+
+  const highlightedColumnIndices = useMemo(
+    () => selectedDependency.map((attribute) => attribute.column.index),
+    [selectedDependency]
+  );
+
+  const pageClass = styles.page;
+
+  const containerClass = styles.content;
+
+  console.log(pageClass, containerClass)
+  return (
+    <><h5 className={styles.header}>Dataset Snippet</h5>
+      <ScrollableTable
+        className={styles.table}
+        header={snippet?.header || []}
+        data={snippet?.rows || []}
+        highlightColumnIndices={highlightedColumnIndices}
+        onScroll={handleScrollToBottom}
+      />
+    </>
+  );
+};
+
+
+export default ReportsSnippet;
